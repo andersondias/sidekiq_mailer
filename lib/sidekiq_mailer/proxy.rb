f0c19e68aefa5dac6b0846ee2f27a2b96c8555ca
Anderson Dias <andersondaraujo@gmail.com>
2012-10-09 16:18:13 -0300

    Refactor and removing comments

--- lib/sidekiq_mailer/proxy.rb
+++ lib/sidekiq_mailer/proxy.rb
@@ -12,36 +12,8 @@ class Sidekiq::Mailer::Proxy
   end
 
   def deliver
-    params = {
-      'class' => Sidekiq::Mailer::Worker,
-      'args' => [@mailer_class.to_s, @method_name, *@args]
-    }
-    Sidekiq::Mailer::Worker.client_push(params.merge(@mailer_class.get_sidekiq_options))
+    Sidekiq::Mailer::Worker.client_push(to_sidekiq)
   end
-
-  # def deliver_at(time)
-  #   return deliver! if environment_excluded?
-
-  #   unless resque.respond_to? :enqueue_at
-  #     raise "You need to install resque-scheduler to use deliver_at"
-  #   end
-
-  #   if @mailer_class.deliver?
-  #     resque.enqueue_at(time, @mailer_class, @method_name, *@args)
-  #   end
-  # end
-
-  # def deliver_in(time)
-  #   return deliver! if environment_excluded?
-
-  #   unless resque.respond_to? :enqueue_in
-  #     raise "You need to install resque-scheduler to use deliver_in"
-  #   end
-
-  #   if @mailer_class.deliver?
-  #     resque.enqueue_in(time, @mailer_class, @method_name, *@args)
-  #   end
-  # end
 
   def deliver!
     actual_message.deliver!
@@ -49,5 +21,13 @@ class Sidekiq::Mailer::Proxy
 
   def method_missing(method_name, *args)
     actual_message.send(method_name, *args)
+  end
+
+  def to_sidekiq
+    params = {
+      'class' => Sidekiq::Mailer::Worker,
+      'args' => [@mailer_class.to_s, @method_name, *@args]
+    }
+    params.merge(@mailer_class.get_sidekiq_options)
   end
 end
